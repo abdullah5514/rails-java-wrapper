f724c2b3517999f29838f9b0dcb6071f016d9a51
Muhammad Abdullah <mabdullah5514@yahoo.com>
2018-12-19 19:48:26 +0500

    Initial Commit

---
+++ lib/rails/java/wrapper.rb
@@ -0,0 +1,12 @@
+require "rails/java/wrapper/version"
+
+module Rails
+  module Java
+    module Wrapper
+      class Error < StandardError; end
+      # Your code goes here...
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
